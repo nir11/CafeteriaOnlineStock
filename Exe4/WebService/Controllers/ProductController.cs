4a2441e780efc6c926269f093ef39bd4043701fb
Nir Almog <nir.almog90@gmail.com>
2018-10-17 17:43:35 +0300

    update

---
+++ Exe4/WebService/Controllers/ProductController.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using ClassLibrary;
+
+namespace WebService.Controllers
+{
+    public class ProductController : ApiController
+    {
+        // GET api/<controller>
+        [Route("api/product/{productName}")]
+        public dynamic Get(string productName)
+        {
+            CafeteriaDB db = new CafeteriaDB();
+
+            return db.Products.Where(x => x.name == productName).Select(y => new
+            {
+                number = y.number,
+                inv = y.inv,
+                price = y.price,
+            }).ToList();
+        }
+
+
+        // GET api/<controller>/5
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/<controller>
+        public void Post([FromBody]string value)
+        {
+        }
+
+        // PUT api/<controller>/5
+        [Route("api/product/{productInv}")]
+        public void Put(int productInv, [FromBody]string productName)
+        {
+
+            CafeteriaDB db = new CafeteriaDB();
+            Product p = db.Products.SingleOrDefault(x => x.name == productName);
+
+            if (p != null)
+            {
+                p.inv = productInv;
+                db.SaveChanges();
+            }
+
+        }
+
+        // DELETE api/<controller>/5
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ Exe4/WebService/Controllers/StudentController.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using ClassLibrary;
+
+namespace WebService.Controllers
+{
+    public class StudentController : ApiController
+    {
+        // GET api/<controller>
+        [Route("api/student/{studentName}/{studentId}")]
+        public dynamic Get(string studentName, int studentId)
+        {
+
+            CafeteriaDB db = new CafeteriaDB();
+            if(db.Students.Any(x => x.id == studentId && x.name == studentName))
+                return Request.CreateResponse(HttpStatusCode.OK, true);
+            return Request.CreateResponse(HttpStatusCode.NotFound, false);
+        }
+
+        // GET api/<controller>/5
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/<controller>
+        public void Post([FromBody]string value)
+        {
+        }
+
+        // PUT api/<controller>/5
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE api/<controller>/5
+        public void Delete(int id)
+        {
+        }
+    }
+}
